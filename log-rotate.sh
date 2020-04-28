0d9fc0f5f4b121ad36edf8cc6e77c7d727b0d632
Roy Thia <roy.thia@oss.com.ph>
2020-04-28 22:30:51 +0800

    update

--- log-rotate.sh
+++ log-rotate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 current_time=$(date +"%d-%m-%Y_%H-%M-%S")
 
-cp /var/log/shadowsocks.log /var/log/SVR-OSS-CALLAWAY/${current_time}.log && /etc/init.d/shadowsocks-python restart
+mv /var/log/shadowsocks.log /var/log/SVR-OSS-CALLAWAY/${current_time}.log && /etc/init.d/shadowsocks-python restart
